44be7dfbca071f3a2a7eb75defd2e7373f3b78d6
Riki225 <79812982+Riki225@users.noreply.github.com>
2024-09-14 17:20:56 +1000

    Removed comments

--- src/app/components/FormSubmitButton.tsx
+++ src/app/components/FormSubmitButton.tsx
@@ -36,16 +36,3 @@ export default function FormSubmitButton(
     </button>
   );
 }
-
-// "use client";
-
-// import { useFormStatus } from "react-dom";
-// import LoadingButton from "./LoadingButton";
-
-// export default function FormSubmitButton(
-//   props: React.ButtonHTMLAttributes<HTMLButtonElement>,
-// ) {
-//   const { pending } = useFormStatus();
-
-//   return <LoadingButton {...props} type="submit" loading={pending} />;
-// }
