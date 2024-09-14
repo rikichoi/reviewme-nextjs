1d0a86c89dd3a90174b216ed7088010903b6666e
Riki225 <79812982+Riki225@users.noreply.github.com>
2024-09-14 15:51:37 +1000

    Created responsive filter sidebar with skeleton for filtering review server action

---
+++ src/app/components/FilterSidebar.tsx
@@ -0,0 +1,84 @@
+import React from "react";
+import {
+  categoryFilterOptions,
+  locationFilterOptions,
+} from "../lib/filter-types";
+
+async function filterReviews(formData: FormData) {
+  "use server";
+  const values = Object.fromEntries(formData.entries());
+  
+  console.log(values);
+}
+
+export default async function FilterSidebar() {
+  return (
+    <div className="sticky top-0 w-full border-b-2 lg:border-2 p-4 lg:rounded-lg lg:bg-white lg:p-3 lg:w-fit">
+      <form action={filterReviews} className="flex flex-col gap-3">
+        <div className="flex flex-col gap-1">
+          <label htmlFor="query" className="font-bold tracking-tight">
+            Search
+          </label>
+          <input
+            id="query"
+            name="query"
+            className="border-2 rounded-lg p-1"
+            placeholder="Title, category, etc..."
+          />
+        </div>
+        <div className="flex flex-col gap-1">
+          <label htmlFor="location" className="font-bold tracking-tight">
+            Location
+          </label>
+          <select
+            id="location"
+            name="location"
+            className="border-2 rounded-lg p-1 bg-white"
+            defaultValue={"All locations"}
+          >
+            {locationFilterOptions.map((location) => (
+              <option
+                key={location}
+                value={location.includes("All locations") ? "" : location}
+              >
+                {location}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="flex flex-col gap-1">
+          <label htmlFor="category" className="font-bold tracking-tight">
+            Category
+          </label>
+          <select
+            id="category"
+            name="category"
+            className="border-2 rounded-lg p-1 bg-white"
+            defaultValue={"All categories"}
+          >
+            {categoryFilterOptions.map((category) => (
+              <option
+                key={category}
+                value={category.includes("All categories") ? "" : category}
+              >
+                {category}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="flex gap-2">
+          <input id="verified" name="verified" type="checkbox"></input>
+          <label htmlFor="verified" className="font-bold tracking-tight">
+            Verified
+          </label>
+        </div>
+        <button
+          type="submit"
+          className="rounded-lg hover:bg-zinc-800 bg-zinc-950 text-white p-2 font-bold tracking-tight"
+        >
+          Filter reviews
+        </button>
+      </form>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,54 @@
+import FilterSidebar from "./components/FilterSidebar";
+
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <h1 className="text-4xl font-bold">Best in Review Site</h1>
-      <p className="text-lg">
-        Review Board for anything. Share your thoughts and experiences with
-        others.
-      </p>
-    </div>
+    <main className="flex flex-col items-center justify-items-center min-h-[2000px] gap-3 bg-[#fcfbf3]">
+      <div className="flex lg:flex-col items-center gap-3 w-full justify-center p-12 bg-white">
+        <h1 className="text-3xl lg:text-5xl tracking-tight font-bold">
+          Best in Review Site
+        </h1>
+        <div className="group relative">
+          <div className="flex items-center gap-2">
+            <span className="text-lg hidden lg:block text-zinc-600">
+              Compare the best companies in this category
+            </span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              className="lucide lucide-info"
+            >
+              <circle cx="12" cy="12" r="10" />
+              <path d="M12 16v-4" />
+              <path d="M12 8h.01" />
+            </svg>
+          </div>
+          <div className="hidden border-2 group-hover:flex flex-col grow right-0 bg-white p-4 min-w-96 absolute gap-3 rounded-lg drop-shadow-xl mt-1">
+            <p className="text-xs">
+              Companies on ReviewMe can choose relevant categories to describe
+              their industry, products, or services.
+            </p>
+            <p className="text-xs">
+              All companies in a category are eligible to be best in that
+              category if they’re actively asking for reviews and have received
+              25 or more reviews in the last 12 months.
+            </p>
+            <p className="text-xs">
+              Some companies on ReviewMe aren’t eligible to be best in a
+              category, and others aren’t on ReviewMe at all.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="w-full flex flex-col lg:flex-row">
+        <FilterSidebar />
+      </div>
+    </main>
   );
 }
