891a4f606b8668fba06503f5918e061485d71668
Riki225 <79812982+Riki225@users.noreply.github.com>
2024-09-22 18:30:47 +1000

    Updated TODO

--- src/app/post-comment/[id]/page.tsx
+++ src/app/post-comment/[id]/page.tsx
@@ -10,6 +10,7 @@ export default function PostCommentPage({
   params: { id },
 }: PostCommentPageProps) {
   return (
+    // TODO: Implement functionality to post a comment about the review using React Hook Form and Zod validation!
     <div className="max-w-3xl bg-white mx-auto my-10 p-8 shadow border">
       <h1 className="text-lg font-semibold tracking-tighter">
         Post a comment about the review!
