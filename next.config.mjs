fb0958a9d89d70866b00d5a5582c945119dae32a
Riki225 <79812982+Riki225@users.noreply.github.com>
2024-09-24 17:00:21 +1000

    Added validation schema for signup and login

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [{ hostname: "zumfm6boirlwmnsj.public.blob.vercel-storage.com" }],
+    remotePatterns: [
+      { hostname: "zumfm6boirlwmnsj.public.blob.vercel-storage.com" },
+    ],
   },
+  serverExternalPackages: ["@node-rs/argon2"],
 };
 
 export default nextConfig;
