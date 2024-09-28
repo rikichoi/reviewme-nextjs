c23a718990c49e9ebadf9886c02bdece5d791c87
Riki225 <79812982+Riki225@users.noreply.github.com>
2024-09-28 19:06:34 +1000

    Added user details to Review [id] page

--- src/app/reviews/[id]/Review.tsx
+++ src/app/reviews/[id]/Review.tsx
@@ -2,12 +2,15 @@ import Image from "next/image";
 import React from "react";
 import placeholderIcon from "@/assets/user-icon-placeholder.png";
 import { Badge } from "@/components/ui/badge";
+import { formatConciseDate } from "@/lib/utils";
+import { ReviewWithUser } from "@/lib/types";
+
 
 type ReviewProps = {
-  description: string;
+  reviewDetails: ReviewWithUser;
 };
 
-export default function Review({ description }: ReviewProps) {
+export default function Review({ reviewDetails }: ReviewProps) {
   return (
     <div className="border bg-white rounded-lg p-6 flex items-center mb-2">
       <div className="flex flex-col gap-5 w-full">
@@ -20,14 +23,18 @@ export default function Review({ description }: ReviewProps) {
             height={300}
           />
           {/* TODO: Implement user and time from backend */}
-          <p>*username*</p>
-          <p>*time*</p>
+          <p>{reviewDetails.User?.displayName || "Anonymous User"}</p>
+          <p>{formatConciseDate(reviewDetails.createdAt)}</p>
           <div className="flex flex-1 justify-end">
             {/* TODO: explore other options to prevent badge/child div from growing in a flex container */}
-            <Badge className="bg-[#3db578] text-black font-bold tracking-wide h-fit hover:bg-[#3db578] hover:cursor-default">MAIN REVIEW</Badge>
+            <Badge className="bg-[#3db578] text-black font-bold tracking-wide h-fit hover:bg-[#3db578] hover:cursor-default">
+              MAIN REVIEW
+            </Badge>
           </div>
         </div>
-        <div dangerouslySetInnerHTML={{ __html: description }}></div>
+        <div
+          dangerouslySetInnerHTML={{ __html: reviewDetails.description }}
+        ></div>
       </div>
     </div>
   );
