e0c7d622d1aa6875d8bce0a8c5d332d0a3a34c32
Riki225 <79812982+Riki225@users.noreply.github.com>
2024-10-03 16:50:47 +1000

    updated error page for review page and made loading skeleton color darker

--- src/app/(main)/reviews/[id]/error.tsx
+++ src/app/(main)/reviews/[id]/error.tsx
@@ -16,7 +16,7 @@ export default function Error({
 
   return (
     <div className="justify-center text-center items-center flex flex-col py-5 gap-12">
-      <h2 className="text-lg md:text-4xl ">{error.message}</h2>
+      <h2 className="text-lg md:text-4xl ">Review not found.</h2>
       <button
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
         onClick={() => router.back()}
